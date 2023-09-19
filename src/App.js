f9a8d2f926a633882b893af735966a1cb3055d57
Darren0422 <134398985+Darren0422@users.noreply.github.com>
2023-09-19 12:50:15 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import DisplayHeader from "./Components/Header";
+import DisplayHelp from "./Components/Help";
+import Game from "./Components/Game";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <DisplayHeader />
+      </div>
+
+      <div className="game">
+        <Game />
+      </div>
+
+      <div className="help">
+        <DisplayHelp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Help.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function DisplayHelp() {
+  // Define a state variable to track whether the rules should be displayed
+  const [showRules, setShowRules] = useState(false);
+
+  // Function to toggle the visibility of the rules
+  const toggleRules = () => {
+    setShowRules(!showRules);
+  };
+
+  // When showrules is true, the text is displayed and when its not, it is hidden
+  return (
+    <div className="Help-container">
+      <div className="Help-content">
+        <button onClick={toggleRules}> Help </button>
+
+        {showRules && (
+          <div className="Help-textbox">
+            <h3>Rules:</h3>
+            <p>
+              The objective of Hangman is to guess the secret word correctly
+              before you run out of attempts
+            </p>
+            <p>
+              The secret word or phrase is represented by a row of underscores,
+              with each underscore representing a letter in the word or phrase.{" "}
+            </p>
+            <p>
+              If a player guesses a letter that is in the word or phrase, all
+              instances of that letter are revealed in the display.{" "}
+            </p>
+            <p>
+              If a player guesses a letter that is not in the word or phrase, a
+              part of the hangman is drawn.
+            </p>
+            <p>You have 6 attempts.</p>
+            <p>Goodluck!</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default DisplayHelp;
